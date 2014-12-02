802040d336dd35b711e7f1bc137b647548f550b9
apopping <attila.popping@icrar.org>
2014-12-02 15:22:44 +0800

    threshold finder does not complain for NaN's
    An intermediate mask is added to replace the NaN’s with zeros during
    the threshold finder, so that it does not give warnings

--- sofia/threshold_filter.py
+++ sofia/threshold_filter.py
@@ -14,17 +14,28 @@ from functions import *
 # a function to a simple threshold filter and write out a mask:
 
 def filter(mask, cube, header, clipMethod, threshold, rmsMode, verbose):
-	if clipMethod == 'relative':
-		# determine the clip level
+    if clipMethod == 'relative':
+        # determine the clip level
 		# Measure noise in original cube
 		# rms = GetRMS(cube,rmsmode=rmsMode,zoomx=1,zoomy=1,zoomz=100000,verb=verbose,nrbins=100000)
-		rms = GetRMS(cube, rmsMode=rmsMode, zoomx=1, zoomy=1, zoomz=1, verbose=verbose)
-		print 'Estimated rms = ', rms
-		clip = threshold * rms
-	if clipMethod == 'absolute':
-		clip = threshold
-	print 'using clip threshold: ', clip
+        rms = GetRMS(cube, rmsMode=rmsMode, zoomx=1, zoomy=1, zoomz=1, verbose=verbose)
+        print 'Estimated rms = ', rms
+        clip = threshold * rms
+    if clipMethod == 'absolute':
+        clip = threshold
+    print 'using clip threshold: ', clip
 	#return ((cube >= clip)+(cube <= -1*clip))
-	np.logical_or(mask, (np.greater_equal(cube, clip) + np.less_equal(cube, -clip)), mask)
+
+   
+    # check whether there are NaNs
+    nan_mask = np.isnan(cube)
+    found_nan=nan_mask.sum()
+    if found_nan:
+        cube=np.nan_to_num(cube)
+        np.logical_or(mask, (np.greater_equal(cube, clip) + np.less_equal(cube, -clip)), mask)
+        cube[nan_mask]=np.nan
+    else:
+        np.logical_or(mask, (np.greater_equal(cube, clip) + np.less_equal(cube, -clip)), mask)
+
 	
-	return 
+    return 
