fb93371633bbf9a23eedcff74adbd0f8d000abeb
paoloserra <paolo.serra@csiro.au>
2014-12-17 12:09:16 +1100

    Update smooth_cube.py

--- sofia/smooth_cube.py
+++ sofia/smooth_cube.py
@@ -35,21 +35,21 @@ def smooth(indata, kernel, edgeMode, kernelX, kernelY, kernelZ):
 			print 'The smoothing type is: Gaussian'
 			print 'The standard deviation for the Gaussian kernel (x,y,z) is: ', kernelX, kernelY, kernelZ
 			print 'The edge is handled as:', edgeMode
-			outdata=ndimage.filters.gaussian_filter(indata, sigma=(kernelZ,kernelX,kernelY), mode=edgeMode)
+			outdata=ndimage.filters.gaussian_filter(outdata, sigma=(kernelZ,kernelX,kernelY), mode=edgeMode)
 		elif type == "b" or kernel == "boxcar":
 			print 'The smoothing type is: Boxcar'
 			print 'The size of the uniform filter is (x,y,z) is: ', kernelX, kernelY, kernelZ
 			print 'The edge is handled as:', edgeMode
-			outdata=ndimage.filters.uniform_filter(indata, size=(kernelZ,kernelX,kernelY), mode=edgeMode)
+			outdata=ndimage.filters.uniform_filter(outdata, size=(kernelZ,kernelX,kernelY), mode=edgeMode)
 		elif type == "m" or kernel == "median":
 			if kernelX == 0 or kernelY == 0 or kernelZ == 0:
 				sys.stderr.write("WARNING: cannot determine median over kernel length zero; no smoothing is applied.\n")
-				outdata = indata
+				#outdata = indata
 			else:
 				print 'The smoothing type is: Median'
 				print 'The size of the filter (x,y,z) is: ', kernelX, kernelY, kernelZ
 				print 'The edge is handled as:', edgeMode
-				outdata=ndimage.filters.median_filter(indata, size=(kernelZ,kernelX,kernelY), mode=edgeMode)
+				outdata=ndimage.filters.median_filter(outdata, size=(kernelZ,kernelX,kernelY), mode=edgeMode)
 		else:
 			sys.stderr.write("ERROR: Smoothing type not recognised.\n")
 			sys.stderr.write("       The program continues without applying any smoothing.\n")
