7aab8583ed63765001455be35c0a412fc43b4750
paoloserra <paolo.serra@csiro.au>
2015-03-24 16:39:37 -0400

    regrid before making moment images if cellscal=1/f

--- src/writemoment2/setup_writemoment2.py
+++ src/writemoment2/setup_writemoment2.py
@@ -1,7 +1,11 @@
+import numpy
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 setup(
   name = 'New writemoment module',
   ext_modules = cythonize("writemoment2.pyx"),
+  include_dirs = [numpy.get_include()],
 )
