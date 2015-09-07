1d81f6417f9acb8d6c9fccd6c025c2a5651ba79b
paoloserra <paolo.serra@csiro.au>
2015-09-07 16:42:45 +1000

    added par to measure noise using subset of voxels
    The parameter is called "sample" and is an argument of the function GetRMS

--- sofia/functions.py
+++ sofia/functions.py
@@ -11,7 +11,7 @@ import scipy.ndimage as nd
 def GaussianNoise(F,N0,s0):
     return N0*np.exp(-F**2/2/s0**2)
 
-def GetRMS(cube,rmsMode='negative',zoomx=1,zoomy=1,zoomz=10000,nrbins=10000,verbose=0,min_hist_peak=0.05):
+def GetRMS(cube,rmsMode='negative',zoomx=1,zoomy=1,zoomz=1,nrbins=10000,verbose=0,min_hist_peak=0.05,sample=1):
 	sh=cube.shape
 
         if len(sh) == 2:
@@ -24,14 +24,14 @@ def GetRMS(cube,rmsMode='negative',zoomx=1,zoomy=1,zoomz=10000,nrbins=10000,verb
 	y0,y1=int(mt.ceil((1-1./zoomy)*sh[1]/2)),int(mt.floor((1+1./zoomy)*sh[1]/2))+1
 	z0,z1=int(mt.ceil((1-1./zoomz)*sh[0]/2)),int(mt.floor((1+1./zoomz)*sh[0]/2))+1
 	if verbose: print '    Estimating rms on subcube (x,y,z zoom = %.0f,%.0f,%.0f) ...'%(zoomx,zoomy,zoomz)
-	if verbose: print '    ... Subcube shape is',cube[z0:z1,y0:y1,x0:x1].shape,'...'
+	if verbose: print '    ... Subcube shape is',cube[z0:z1:sample,y0:y1:sample,x0:x1:sample].shape,'...'
 
 	if rmsMode=='negative':
 		nrbins=max(100,int(mt.ceil(float(np.array(cube.shape).prod())/1e+5))) # overwrites nrbins value!!!
 		cubemin=np.nanmin(cube)
 		bins=np.arange(cubemin,abs(cubemin)/nrbins-1e-12,abs(cubemin)/nrbins)
 		fluxval=(bins[:-1]+bins[1:])/2
-		rmshisto=np.histogram(cube[z0:z1,y0:y1,x0:x1],bins=bins)[0]
+		rmshisto=np.histogram(cube[z0:z1:sample,y0:y1:sample,x0:x1:sample],bins=bins)[0]
 
 		nrsummedbins=0
 		while rmshisto[-nrsummedbins-1:].sum()<min_hist_peak*rmshisto.sum():
@@ -41,15 +41,15 @@ def GetRMS(cube,rmsMode='negative',zoomx=1,zoomy=1,zoomz=10000,nrbins=10000,verb
 			nrbins/=(nrsummedbins+1)
 			bins=np.arange(cubemin,abs(cubemin)/nrbins-1e-12,abs(cubemin)/nrbins)
 			fluxval=(bins[:-1]+bins[1:])/2
-			rmshisto=np.histogram(cube[z0:z1,y0:y1,x0:x1],bins=bins)[0]
+			rmshisto=np.histogram(cube[z0:z1:sample,y0:y1:sample,x0:x1:sample],bins=bins)[0]
 
 		rms=abs(optimize.curve_fit(GaussianNoise,fluxval,rmshisto,p0=[rmshisto.max(),-fluxval[rmshisto<rmshisto.max()/2].max()*2/2.355])[0][1])
 	elif rmsMode=='mad':
-                rms=sp.stats.nanmedian(abs(cube[z0:z1,y0:y1,x0:x1]-sp.stats.nanmedian(cube[z0:z1,y0:y1,x0:x1],axis=None)),axis=None)/0.6745
+                rms=sp.stats.nanmedian(abs(cube[z0:z1:sample,y0:y1:sample,x0:x1:sample]-sp.stats.nanmedian(cube[z0:z1:sample,y0:y1:sample,x0:x1:sample],axis=None)),axis=None)/0.6745
 
             
 	elif rmsMode=='std':
-                rms=np.nanstd(cube[z0:z1,y0:y1,x0:x1],axis=None,dtype=np.float64)
+                rms=np.nanstd(cube[z0:z1:sample,y0:y1:sample,x0:x1:sample],axis=None,dtype=np.float64)
 	if verbose: print '    ... %s rms = %.2e (data units)'%(rmsMode,rms)
 
 
