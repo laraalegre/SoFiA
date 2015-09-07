ee8547825b81d79c1373ad1dd3012555a901c0ba
paoloserra <paolo.serra@csiro.au>
2015-09-07 15:58:12 +1000

    commented out old functions

--- sofia/pyfind.py
+++ sofia/pyfind.py
@@ -11,12 +11,12 @@ def SizeFilter(mskt,sfx,sfy,sfz,sbx,sby,sbz,zt,sizeFilter,edgeMode='constant',ve
 	mskt[mskt>=sizeFilter]=1
 	return mskt
 
-def MaskedCube(incube,msk,replace_value):
-	maskedcube=np.copy(incube)
-	maskedcube[msk]=np.sign(incube[msk])*np.minimum(abs(incube[msk]),replace_value)
-	# this only decreases the absolute value of voxels already in the mask, or leaves it unchanged
-	# if already lower than replace_value; the sign is unchanged
-	return maskedcube
+#def MaskedCube(incube,msk,replace_value):
+#	maskedcube=np.copy(incube)
+#	maskedcube[msk]=np.sign(incube[msk])*np.minimum(abs(incube[msk]),replace_value)
+#	# this only decreases the absolute value of voxels already in the mask, or leaves it unchanged
+#	# if already lower than replace_value; the sign is unchanged
+#	return maskedcube
 
 def SortKernels(kernels):
 	# Sorting kernels
@@ -36,89 +36,89 @@ def SortKernels(kernels):
 				velfshape[-1].append(ii[3])
 	return uniquesky,velsmooth,velfshape
 
-def SCfinder(cube,header,t0,kernels=[[0,0,0,'b'],],threshold=3.5,sizeFilter=0,maskScaleXY=2.,maskScaleZ=2.,kernelUnit='pixel',edgeMode='constant',rmsMode='negative',verbose=0):
-	# Create binary mask array
-	msk=np.zeros(cube.shape,'bool')
-	found_nan=np.isnan(cube).sum()
-
-	# Measure noise in original cube
-	rms=GetRMS(cube,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
-
-	# Sort kernels
-	uniquesky,velsmooth,velfshape=SortKernels(kernels)
-
-	# Loop over all xy kernels
-	for jj in range(len(uniquesky)):
-		[kx,ky]=uniquesky[jj]
-		if kernelUnit=='world' or kernelUnit=='w':
-			kx=abs(float(kx)/header['cdelt1']/3600)
-			ky=abs(float(ky)/header['cdelt2']/3600)
-		if verbose: 
-			print "\n--- %.3f seconds since start"%(time()-t0)
-			print '    Filter %2.0f %2.0f %2.0f %s ...'%(kx,ky,0,'-')
-		sys.stdout.flush()
-
-		mskxy=np.zeros(cube.shape,'bool')
-
-		# Get previous xy kernel
-		if jj: [kxold,kyold]=uniquesky[jj-1]
-		else: [kxold,kyold]=[0,0]
-
-		# Gaussian angular smoothing of *clipped* cube if needed
-		if kx+ky:
-			# smooth starting from the latest xy cube (clipped to the detection threshold times maskScaleXY)
-			cubexy=nd.filters.gaussian_filter(np.clip(cubexy,-maskScaleXY*threshold*rmsxy,maskScaleXY*threshold*rmsxy),[0,mt.sqrt(ky**2-kyold**2)/2.355,mt.sqrt(kx**2-kxold**2)/2.355],mode=edgeMode)
-			if found_nan: cubexy[np.isnan(cube)]=np.nan
-			rmsxy=GetRMS(cubexy,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
-			if found_nan: cubexy=np.nan_to_num(cubexy)
-		elif found_nan: cubexy,rmsxy=np.nan_to_num(cube),rms
-		else: cubexy,rmsxy=np.copy(cube),rms
-
-		# Loop over all z kernels
-		for ii in range(len(velsmooth[jj])):
-			kz=velsmooth[jj][ii]
-			kt=velfshape[jj][ii]
-			if kernelUnit=='world' or kernelUnit=='w': kz=abs(float(kz)/header['cdelt3']*1000)
-
-			# Velocity smoothing of *clipped* cube if needed
-			if kz:
-				if verbose: 
-					print "\n--- %.3f seconds since start"%(time()-t0)
-					print '    Filter %2.0f %2.0f %2.0f %s ...'%(kx,ky,kz,kt)
-				sys.stdout.flush()
-				if not ii:
-					rmsxyz=rmsxy/mt.sqrt(kz)
-					print '!!!'
-				# smooth starting from the latest xy cube
-				# before smoothing voxels already detected at the current angular resolution are brought down to +/-maskScaleZ*rmsxyz*threshold
-				if kt=='b': cubexyz=nd.filters.uniform_filter1d(MaskedCube(cubexy,mskxy,maskScaleZ*rmsxyz*threshold),kz,axis=0,mode=edgeMode)
-				elif kt=='g': cubexyz=nd.filters.gaussian_filter1d(MaskedCube(cubexy,mskxy,maskScaleZ*rmsxyz*threshold),kz/2.355,axis=0,mode=edgeMode)
-				if found_nan: cubexyz[np.isnan(cube)]=np.nan
-				rmsxyz=GetRMS(cubexyz,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
-				if found_nan: cubexyz=np.nan_to_num(cubexyz)
-			else: cubexyz,rmsxyz=cubexy,rmsxy
-
-			# Add detected voxels to mask
-			if sizeFilter:
-				if verbose: print '      Adding detected voxels to xy mask with size filtering'
-				sys.stdout.flush()
-				# Get beam FWHM in pixels
-				if 'BMAJ' in header.keys(): bmaj=header['bmaj']/abs(header['cdelt2']) # assumed to be along y axis
-				else: bmaj=0
-				if 'BMIN' in header.keys(): bmin=header['bmin']/abs(header['cdelt1']) # assumed to be along x axis
-				else: bmin=bmaj
-				mskxy=mskxy+SizeFilter(((cubexyz>=threshold*rmsxyz)+(cubexyz<=-threshold*rmsxyz)).astype('float32'),kx,ky,kz,bmin,bmaj,3,kt,sizeFilter,edgeMode=edgeMode,verbose=0).astype('bool')
-			else:
-				if verbose: print '      Adding detected voxels directly to xy mask ...'
-				sys.stdout.flush()
-				mskxy=mskxy+(cubexyz>=threshold*rmsxyz)+(cubexyz<=-threshold*rmsxyz)
-
-		if verbose: print '    Adding xy mask to final mask ...'
-		sys.stdout.flush()
-		msk=msk+mskxy
-
-	msk[np.isnan(cube)]=False
-	return msk
+#def SCfinder(cube,header,t0,kernels=[[0,0,0,'b'],],threshold=3.5,sizeFilter=0,maskScaleXY=2.,maskScaleZ=2.,kernelUnit='pixel',edgeMode='constant',rmsMode='negative',verbose=0):
+#	# Create binary mask array
+#	msk=np.zeros(cube.shape,'bool')
+#	found_nan=np.isnan(cube).sum()
+#
+#	# Measure noise in original cube
+#	rms=GetRMS(cube,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
+#
+#	# Sort kernels
+#	uniquesky,velsmooth,velfshape=SortKernels(kernels)
+#
+#	# Loop over all xy kernels
+#	for jj in range(len(uniquesky)):
+#		[kx,ky]=uniquesky[jj]
+#		if kernelUnit=='world' or kernelUnit=='w':
+#			kx=abs(float(kx)/header['cdelt1']/3600)
+#			ky=abs(float(ky)/header['cdelt2']/3600)
+#		if verbose: 
+#			print "\n--- %.3f seconds since start"%(time()-t0)
+#			print '    Filter %2.0f %2.0f %2.0f %s ...'%(kx,ky,0,'-')
+#		sys.stdout.flush()
+#
+#		mskxy=np.zeros(cube.shape,'bool')
+#
+#		# Get previous xy kernel
+#		if jj: [kxold,kyold]=uniquesky[jj-1]
+#		else: [kxold,kyold]=[0,0]
+#
+#		# Gaussian angular smoothing of *clipped* cube if needed
+#		if kx+ky:
+#			# smooth starting from the latest xy cube (clipped to the detection threshold times maskScaleXY)
+#			cubexy=nd.filters.gaussian_filter(np.clip(cubexy,-maskScaleXY*threshold*rmsxy,maskScaleXY*threshold*rmsxy),[0,mt.sqrt(ky**2-kyold**2)/2.355,mt.sqrt(kx**2-kxold**2)/2.355],mode=edgeMode)
+#			if found_nan: cubexy[np.isnan(cube)]=np.nan
+#			rmsxy=GetRMS(cubexy,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
+#			if found_nan: cubexy=np.nan_to_num(cubexy)
+#		elif found_nan: cubexy,rmsxy=np.nan_to_num(cube),rms
+#		else: cubexy,rmsxy=np.copy(cube),rms
+#
+#		# Loop over all z kernels
+#		for ii in range(len(velsmooth[jj])):
+#			kz=velsmooth[jj][ii]
+#			kt=velfshape[jj][ii]
+#			if kernelUnit=='world' or kernelUnit=='w': kz=abs(float(kz)/header['cdelt3']*1000)
+#
+#			# Velocity smoothing of *clipped* cube if needed
+#			if kz:
+#				if verbose: 
+#					print "\n--- %.3f seconds since start"%(time()-t0)
+#					print '    Filter %2.0f %2.0f %2.0f %s ...'%(kx,ky,kz,kt)
+#				sys.stdout.flush()
+#				if not ii:
+#					rmsxyz=rmsxy/mt.sqrt(kz)
+#					print '!!!'
+#				# smooth starting from the latest xy cube
+#				# before smoothing voxels already detected at the current angular resolution are brought down to +/-maskScaleZ*rmsxyz*threshold
+#				if kt=='b': cubexyz=nd.filters.uniform_filter1d(MaskedCube(cubexy,mskxy,maskScaleZ*rmsxyz*threshold),kz,axis=0,mode=edgeMode)
+#				elif kt=='g': cubexyz=nd.filters.gaussian_filter1d(MaskedCube(cubexy,mskxy,maskScaleZ*rmsxyz*threshold),kz/2.355,axis=0,mode=edgeMode)
+#				if found_nan: cubexyz[np.isnan(cube)]=np.nan
+#				rmsxyz=GetRMS(cubexyz,rmsMode=rmsMode,zoomx=1,zoomy=1,zoomz=1,verbose=verbose)
+#				if found_nan: cubexyz=np.nan_to_num(cubexyz)
+#			else: cubexyz,rmsxyz=cubexy,rmsxy
+#
+#			# Add detected voxels to mask
+#			if sizeFilter:
+#				if verbose: print '      Adding detected voxels to xy mask with size filtering'
+#				sys.stdout.flush()
+#				# Get beam FWHM in pixels
+#				if 'BMAJ' in header.keys(): bmaj=header['bmaj']/abs(header['cdelt2']) # assumed to be along y axis
+#				else: bmaj=0
+#				if 'BMIN' in header.keys(): bmin=header['bmin']/abs(header['cdelt1']) # assumed to be along x axis
+#				else: bmin=bmaj
+#				mskxy=mskxy+SizeFilter(((cubexyz>=threshold*rmsxyz)+(cubexyz<=-threshold*rmsxyz)).astype('float32'),kx,ky,kz,bmin,bmaj,3,kt,sizeFilter,edgeMode=edgeMode,verbose=0).astype('bool')
+#			else:
+#				if verbose: print '      Adding detected voxels directly to xy mask ...'
+#				sys.stdout.flush()
+#				mskxy=mskxy+(cubexyz>=threshold*rmsxyz)+(cubexyz<=-threshold*rmsxyz)
+#
+#		if verbose: print '    Adding xy mask to final mask ...'
+#		sys.stdout.flush()
+#		msk=msk+mskxy
+#
+#	msk[np.isnan(cube)]=False
+#	return msk
 
 def SCfinder_mem(cube,header,t0,kernels=[[0,0,0,'b'],],threshold=3.5,sizeFilter=0,maskScaleXY=2.,maskScaleZ=2.,kernelUnit='pixel',edgeMode='constant',rmsMode='negative',verbose=0):
     # Create binary mask array
