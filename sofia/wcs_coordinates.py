430a20f5b05688a8746f6abce6904aeb53973361
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2015-09-22 06:09:28 +0000

    Introduction of IAU-compliant source names This update introduces IAU-compliant source names. Currently, the names are based on positional coordinates only and do not include spectral information yet. Also, the new names are not yet inserted into the catalogue due to an unresolved issue. This will be fixed in a future update.

--- sofia/wcs_coordinates.py
+++ sofia/wcs_coordinates.py
@@ -5,6 +5,22 @@
 # GIPSY header repaired following this description:
 # http://www.astro.rug.nl/software/kapteyn/spectralbackground.html#a-recipe-for-modification-of-nmap-gipsy-fits-data
 
+# -------------------------------------
+# ---- FUNCTION TO IMPLEMENT SGN() ----
+# -------------------------------------
+
+def math_sgn(value):
+	# Note: This does not handle special cases such as ±0, ±∞, etc.,
+	#       but that’s not really needed here anyway.
+	if(value < 0): return -1;
+	return 1;
+
+
+
+
+# --------------------------------------------
+# ---- FUNCTION TO FIX GIPSY FILE HEADERS ----
+# --------------------------------------------
 
 def fix_gipsy_header(header_orig):
 	
@@ -92,12 +108,12 @@ def add_wcs_coordinates(objects,catParNames,catParFormt,catParUnits,Parameters):
 	import sys
 	import numpy as np
 	try:
-	    imp.find_module('astropy')
-	    found = True
+		imp.find_module('astropy')
+		found = True
 	except ImportError: found = False
 	
 	if found:
-		try:	
+		try:
 			from astropy import wcs
 			from astropy.io import fits
 			
@@ -119,54 +135,119 @@ def add_wcs_coordinates(objects,catParNames,catParFormt,catParUnits,Parameters):
 				header = fix_gipsy_header(header)
 				wcsin = Wcsprm(str(header))
 				wcsin.sptr('VOPT-F2W')
-				if header['naxis']==4:
-					objects = np.concatenate((objects,wcsin.p2s(np.concatenate((objects[:,catParNames.index('x'):catParNames.index('x')+3],np.zeros((objects.shape[0],1))),axis=1),0)['world'][:,:-1]),axis=1)
+				if header['naxis'] == 4:
+					objects = np.concatenate((objects, wcsin.p2s(np.concatenate((objects[:, catParNames.index('x'):catParNames.index('x')+3], np.zeros((objects.shape[0], 1))), axis=1),0)['world'][:,:-1]), axis=1)
 				else:
-					objects = np.concatenate((objects,wcsin.p2s(objects[:,catParNames.index('x'):catParNames.index('x')+3],0)['world']),axis=1)
+					objects = np.concatenate((objects, wcsin.p2s(objects[:, catParNames.index('x'):catParNames.index('x')+3], 0)['world']), axis=1)
 				catParUnits = tuple(list(catParUnits) + [str(cc).replace(' ','') for cc in wcsin.cunit])
 				catParNames = tuple(list(catParNames) + [(cc.split('--')[0]).lower() for cc in wcsin.ctype])
 				catParFormt = tuple(list(catParFormt) + ['%15.7e', '%15.7e', '%15.7e'])
 
 			else:
 				# constrain the RA axis reference value CRVAL_ to be between 0 and 360 deg
-				rafound=0
+				rafound = 0
 				for kk in range(header['naxis']):
-					if header['ctype1'][:2]=='RA':
-						rafound=1
+					if header['ctype1'][:2] == 'RA':
+						rafound = 1
 						break
 				if rafound:
-					if header['crval%i'%(kk+1)]<0:
+					if header['crval%i'%(kk + 1)] < 0:
 						sys.stderr.write("WARNING: adding 360 deg to RA reference value.\n")
-						header['crval%i'%(kk+1)]+=360
-					elif header['crval%i'%(kk+1)]>360:
+						header['crval%i'%(kk + 1)] += 360
+					elif header['crval%i'%(kk + 1)] > 360:
 						sys.stderr.write("WARNING: subtracting 360 deg from RA reference value.\n")
-						header['crval%i'%(kk+1)]-=360
+						header['crval%i'%(kk + 1)] -= 360
 
 				wcsin = wcs.WCS(header)
-				xyz=objects[:,catParNames.index('x'):catParNames.index('x')+3].astype(float)
+				xyz = objects[:,catParNames.index('x'):catParNames.index('x')+3].astype(float)
 				if 'cellscal' in header and header['cellscal'] == '1/F':
 					print 'WARNING: CELLSCAL keyword with value 1/F found.'
 					print 'Will take into account varying pixel scale when calculating wcs coordinates.'
-					x0,y0=header['crpix1']-1,header['crpix2']-1
+					x0, y0 = header['crpix1'] - 1, header['crpix2'] - 1
 					# Will calculate the pixscale factor of each channel as:
 					# pixscale = ref_frequency / frequency
-					if header['ctype3']=='VELO-HEL':
-						pixscale=(1-header['crval3']/2.99792458e+8)/(1-(((xyz[:,2]+1)-header['crpix3'])*header['cdelt3']+header['crval3'])/2.99792458e+8)
+					if header['ctype3'] == 'VELO-HEL':
+						pixscale = (1 - header['crval3'] / c) / (1 - (((xyz[:,2] + 1) - header['crpix3']) * header['cdelt3'] + header['crval3']) / c)
 					else:
-						sys.stderr.write("WARNING: Cannot convert axis3 coordinates to frequency. Will ignore the effect of CELLSCAL = 1/F.\n")
-						pixscale=1.
-					xyz[:,0]=(xyz[:,0]-x0)*pixscale+x0
-					xyz[:,1]=(xyz[:,1]-y0)*pixscale+y0
-				if header['naxis']==4: objects=np.concatenate((objects,wcsin.wcs_pix2world(np.concatenate((xyz,np.zeros((objects.shape[0],1))),axis=1),0)[:,:-1]),axis=1)
-				else: objects=np.concatenate((objects,wcsin.wcs_pix2world(xyz,0)),axis=1)
+						sys.stderr.write("WARNING: Cannot convert 3rd axis coordinates to frequency. Will ignore the effect of CELLSCAL = 1/F.\n")
+						pixscale = 1.
+					xyz[:,0] = (xyz[:,0] - x0) * pixscale + x0
+					xyz[:,1] = (xyz[:,1] - y0) * pixscale + y0
+				if header['naxis'] == 4: objects = np.concatenate((objects, wcsin.wcs_pix2world(np.concatenate((xyz, np.zeros((objects.shape[0], 1))), axis=1), 0)[:,:-1]), axis=1)
+				else: objects = np.concatenate((objects, wcsin.wcs_pix2world(xyz, 0)), axis=1)
 				catParUnits = tuple(list(catParUnits) + [str(cc).replace(' ','') for cc in wcsin.wcs.cunit])
 				catParNames = tuple(list(catParNames) + [(cc.split('--')[0]).lower() for cc in wcsin.wcs.ctype])
 				catParFormt = tuple(list(catParFormt) + ['%15.7e', '%15.7e', '%15.7e'])
-			if header['naxis']==4:
+			if header['naxis'] == 4:
 				catParUnits = catParUnits[:-1]
 				catParNames= catParNames[:-1]
 			print "WCS coordinates added to the catalogue."
+			
+			# Create IAU-compliant source name:
+			# WARNING: This currently assumes a regular, three-dimensional data cube!
+			n_src = objects.shape[0];
+			n_par = objects.shape[1];
+			
+			iau_names = np.empty((n_src, 1), dtype="string");
+			
+			if(header["ctype1"][:4] == "RA--"):
+				# Equatorial coordinates; try to figure out equinox:
+				iau_coord = "equ";
+				if("equinox" in header):
+					if(int(header["equinox"]) == 2000): iau_equinox = "J";
+					elif(int(header["equinox"]) == 1950): iau_equinox = "B";
+					else: iau_equinox = "X";
+				elif("epoch" in header):
+					# Assume that EPOCH has been abused to record the equinox:
+					if(int(header["epoch"]) == 2000): iau_equinox = "J";
+					elif(int(header["epoch"]) == 1950): iau_equinox = "B";
+					else: iau_equinox = "X";
+				else:
+					# Unknown equinox:
+					iau_equinox = "X";
+			elif(header["ctype1"][:4] == "GLON"):
+				# Galactic coordinates:
+				iau_coord = "gal";
+				iau_equinox = "G";
+			else:
+				# Unsupported coordinate system:
+				iau_coord = "";
+				iau_equinox = "";
+			
+			for src in xrange(n_src):
+				lon = objects[src][n_par - 3];
+				lat = objects[src][n_par - 2];
 				
+				if(iau_coord == "equ"):
+					ra = lon / 15.0;   # convert assumed degrees to hours
+					ra_h = int(ra);
+					ra_m = int((ra - ra_h) * 60.0);
+					ra_s = (ra - ra_h - (ra_m / 60.0)) * 3600.0;
+					
+					dec_sgn = math_sgn(lat);
+					dec = abs(lat);
+					dec_d = int(dec);
+					dec_m = int((dec - dec_d) * 60.0);
+					dec_s = (dec - dec_d - (dec_m / 60.0)) * 3600.0;
+					
+					iau_pos = "{0:02d}{1:02d}{2:05.2f}".format(ra_h, ra_m, ra_s);
+					if(dec_sgn < 0): iau_pos += "-";
+					else: iau_pos += "+";
+					iau_pos += "{0:02d}{1:02d}{2:04.1f}".format(dec_d, dec_m, dec_s);
+				else:
+					iau_pos = "{0:08.4f}".format(lon);
+					if(lat < 0.0): iau_pos += "-";
+					else: iau_pos += "+";
+					iau_pos += "{0:07.4f}".format(abs(lat));
+				
+				iau_names[src][0] = "SoFiA " + iau_equinox + iau_pos;
+			
+			# NOTE: Insertion of source names disabled due to problem.
+			#objects = np.concatenate((objects, iau_names), axis = 1);
+			#catParUnits = tuple(list(catParUnits) + [""]);
+			#catParNames = tuple(list(catParNames) + ["name"]);
+			#catParFormt = tuple(list(catParFormt) + ["%s"]); ### ALERT THIS STILL DOESN’T WORK!
+			
 		except:
 			sys.stderr.write("WARNING: WCS conversion of parameters failed.\n")
 			
