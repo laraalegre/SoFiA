cleaned up code and fixed mom maps' units
I have improved the way cubelets.py deals with different 3rd-axis types/units.

The new code can deal with a 3rd axis in frequency (as long as the CTYPE3 value includes the string "FREQ" -- case insensitive) and velocity (for CTYPE3 equal to VOPT, VRAD, VELO or FELO -- case insensitive). For these two cases, if no units are given (i.e., no CUNIT3 is present in the header) the code assumes Hz and m/s, respectively, and mom0, 1 and 2 images are scaled accordingly. If the units are m/s or km/s  they are converted to km/s and Hz, respectively, and mom0, 1 and 2 images are scaled accordingly. For all other 3rd-axis types and/or units the code makes no conversion: it multiplies the moment-0 image by CDELT3 and makes use of CUNIT3 if given, else a "std_unit_"+CTYPE3 is reported in the header.

Note that writemoment2.pyx should be made consistent with cubelets.py .

sofia/cubelets.py
@@ -9,119 +9,115 @@ from scipy.ndimage import map_coordinates
 
 
 def regridMaskedChannels(datacube,maskcube,header):
+	maskcubeFlt = maskcube.astype('float')
+	maskcubeFlt[maskcubeFlt>1] = 1
+
+	print 'Regridding...'
+	sys.stdout.flush()
+	from scipy import interpolate
+	z=(np.arange(1.,header['naxis3']+1)-header['crpix3'])*header['cdelt3']+header['crval3']
+	if header['ctype3']=='VELO-HEL':
+		pixscale=(1-header['crval3']/2.99792458e+8)/(1-z/2.99792458e+8)
+	else:
+		sys.stderr.write("WARNING: Cannot convert axis3 coordinates to frequency. Will ignore the effect of CELLSCAL = 1/F.\n")
+		pixscale=np.ones((header['naxis3']))
+	x0,y0=header['crpix1']-1,header['crpix2']-1
+	xs=np.arange(datacube.shape[2],dtype=float)-x0
+	ys=np.arange(datacube.shape[1],dtype=float)-y0
+	for zz in range(datacube.shape[0]):
+		regrid_channel=interpolate.RectBivariateSpline(ys*pixscale[zz],xs*pixscale[zz],datacube[zz])
+		datacube[zz]=regrid_channel(ys,xs)
+		regrid_channel_mask=interpolate.RectBivariateSpline(ys*pixscale[zz],xs*pixscale[zz],maskcubeFlt[zz])
+		maskcubeFlt[zz] = regrid_channel_mask(ys,xs)
   
-  maskcubeFlt = maskcube.astype('float')
-  maskcubeFlt[maskcubeFlt>1] = 1
-  
-  print 'Regridding...'
-  sys.stdout.flush()
-  from scipy import interpolate
-  z=(np.arange(1.,header['naxis3']+1)-header['crpix3'])*header['cdelt3']+header['crval3']
-  if header['ctype3']=='VELO-HEL':
-    pixscale=(1-header['crval3']/2.99792458e+8)/(1-z/2.99792458e+8)
-  else:
-    sys.stderr.write("WARNING: Cannot convert axis3 coordinates to frequency. Will ignore the effect of CELLSCAL = 1/F.\n")
-    pixscale=np.ones((header['naxis3']))
-  x0,y0=header['crpix1']-1,header['crpix2']-1
-  xs=np.arange(datacube.shape[2],dtype=float)-x0
-  ys=np.arange(datacube.shape[1],dtype=float)-y0
-  for zz in range(datacube.shape[0]):
-    regrid_channel=interpolate.RectBivariateSpline(ys*pixscale[zz],xs*pixscale[zz],datacube[zz])
-    datacube[zz]=regrid_channel(ys,xs)
-    regrid_channel_mask=interpolate.RectBivariateSpline(ys*pixscale[zz],xs*pixscale[zz],maskcubeFlt[zz])
-    maskcubeFlt[zz] = regrid_channel_mask(ys,xs)
-  
-  maskMin = maskcubeFlt.min()
-  datacube[abs(maskcubeFlt)<=abs(maskMin)] = np.nan
-  del maskcubeFlt
-  
-  return datacube
+	maskMin = maskcubeFlt.min()
+	datacube[abs(maskcubeFlt)<=abs(maskMin)] = np.nan
+	del maskcubeFlt
+	return datacube
 
 def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOverwrite):
     
-    # strip path variable to get the file name and the directory separately
-    splitroot = outroot.split('/')
-    cubename  = splitroot[-1]
-    if len(splitroot)>1:
-      outputDir = '/'.join(splitroot[:-1])+'/objects/'
-    else:
-      outputDir = './objects/'
+	# strip path variable to get the file name and the directory separately
+	splitroot = outroot.split('/')
+	cubename  = splitroot[-1]
+	if len(splitroot)>1:
+		outputDir = '/'.join(splitroot[:-1])+'/objects/'
+	else:
+		outputDir = './objects/'
       
-    # check if output directory exists and create it if not
-    if os.path.exists(outputDir)==False:
-      os.system('mkdir '+outputDir)
+	# check if output directory exists and create it if not
+	if os.path.exists(outputDir)==False:
+		os.system('mkdir '+outputDir)
 
-    
-    # read all important information (central pixels & values, increments) from the header
-    dX    = header['CDELT1']
-    dY    = header['CDELT2']
-    dZ    = header['CDELT3']
-    cValX = header['CRVAL1']
-    cValY = header['CRVAL2']
-    cValZ = header['CRVAL3']
-    cPixX = header['CRPIX1']-1
-    cPixY = header['CRPIX2']-1
-    cPixZ = header['CRPIX3']-1
-    #specTypeX = header['CTYPE3']
-    #specTypeY = header['CTYPE3']
-    #specUnitY = header['BUNIT']
-    #specUnitX = header['CUNIT3']
-    cubeDim = cube.shape
-    
-    for obj in objects:
-      
-	# centers and bounding boxes 
-	#obj = np.array(objects[rr - 1])
-	Xc = obj[cathead=='x'][0]
-	Yc = obj[cathead=='y'][0]
-	Zc = obj[cathead=='z'][0]
-	Xmin = obj[cathead=='x_min'][0]
-	Ymin = obj[cathead=='y_min'][0]
-	Zmin = obj[cathead=='z_min'][0]
-	Xmax = obj[cathead=='x_max'][0]
-	Ymax = obj[cathead=='y_max'][0]
-	Zmax = obj[cathead=='z_max'][0]
+	# read all important information (central pixels & values, increments) from the header
+	dX    = header['CDELT1']
+	dY    = header['CDELT2']
+	dZ    = header['CDELT3']
+	cValX = header['CRVAL1']
+	cValY = header['CRVAL2']
+	cValZ = header['CRVAL3']
+	cPixX = header['CRPIX1']-1
+	cPixY = header['CRPIX2']-1
+	cPixZ = header['CRPIX3']-1
+	#specTypeX = header['CTYPE3']
+	#specTypeY = header['CTYPE3']
+	#specUnitY = header['BUNIT']
+	#specUnitX = header['CUNIT3']
+	cubeDim = cube.shape
+
+	for obj in objects:
+		# centers and bounding boxes 
+		#obj = np.array(objects[rr - 1])
+		Xc = obj[cathead=='x'][0]
+		Yc = obj[cathead=='y'][0]
+		Zc = obj[cathead=='z'][0]
+		Xmin = obj[cathead=='x_min'][0]
+		Ymin = obj[cathead=='y_min'][0]
+		Zmin = obj[cathead=='z_min'][0]
+		Xmax = obj[cathead=='x_max'][0]
+		Ymax = obj[cathead=='y_max'][0]
+		Zmax = obj[cathead=='z_max'][0]
+		
+		# if center of mass estimation is wrong replace by geometric center
+		if Xc < 0 or Xc > cubeDim[2]-1:  Xc = obj[cathead=='x_geo'][0]
+		if Yc < 0 or Yc > cubeDim[1]-1:  Yc = obj[cathead=='y_geo'][0]
+		if Zc < 0 or Zc > cubeDim[0]-1:  Zc = obj[cathead=='z_geo'][0]
+
+		cPixXNew = int(Xc)
+		cPixYNew = int(Yc)
+		cPixZNew = int(Zc)
 	
-	# if center of mass estimation is wrong replace by geometric center
-	if Xc < 0 or Xc > cubeDim[2]-1:  Xc = obj[cathead=='x_geo'][0]
-	if Yc < 0 or Yc > cubeDim[1]-1:  Yc = obj[cathead=='y_geo'][0]
-	if Zc < 0 or Zc > cubeDim[0]-1:  Zc = obj[cathead=='z_geo'][0]
-    
-        cPixXNew = int(Xc)
-        cPixYNew = int(Yc)
-        cPixZNew = int(Zc)
-	
-	# largest distance of source limits from the center
-	maxX = 2*max(abs(cPixXNew-Xmin),abs(cPixXNew-Xmax))
-	maxY = 2*max(abs(cPixYNew-Ymin),abs(cPixYNew-Ymax))
-	maxZ = 2*max(abs(cPixZNew-Zmin),abs(cPixZNew-Zmax))
-	
-	# calculate the new bounding box for the mass centered cube
-	XminNew = cPixXNew - maxX
-	if XminNew < 0: XminNew = 0
-	YminNew = cPixYNew - maxY
-	if YminNew < 0: YminNew = 0
-	ZminNew = cPixZNew - maxZ
-	if ZminNew < 0: ZminNew = 0
-	XmaxNew = cPixXNew + maxX
-	if XmaxNew > cubeDim[2]-1: XmaxNew = cubeDim[2]-1
-	YmaxNew = cPixYNew + maxY
-	if YmaxNew > cubeDim[1]-1: YmaxNew = cubeDim[1]-1
-	ZmaxNew = cPixZNew + maxZ
-	if ZmaxNew > cubeDim[0]-1: ZmaxNew = cubeDim[0]-1
-	
+		# largest distance of source limits from the center
+		maxX = 2*max(abs(cPixXNew-Xmin),abs(cPixXNew-Xmax))
+		maxY = 2*max(abs(cPixYNew-Ymin),abs(cPixYNew-Ymax))
+		maxZ = 2*max(abs(cPixZNew-Zmin),abs(cPixZNew-Zmax))
+
+		# calculate the new bounding box for the mass centered cube
+		XminNew = cPixXNew - maxX
+		if XminNew < 0: XminNew = 0
+		YminNew = cPixYNew - maxY
+		if YminNew < 0: YminNew = 0
+		ZminNew = cPixZNew - maxZ
+		if ZminNew < 0: ZminNew = 0
+		XmaxNew = cPixXNew + maxX
+		if XmaxNew > cubeDim[2]-1: XmaxNew = cubeDim[2]-1
+		YmaxNew = cPixYNew + maxY
+		if YmaxNew > cubeDim[1]-1: YmaxNew = cubeDim[1]-1
+		ZmaxNew = cPixZNew + maxZ
+		if ZmaxNew > cubeDim[0]-1: ZmaxNew = cubeDim[0]-1
+
 	# calculate the center with respect to the cutout cube
 	cPixXCut = cPixX - XminNew
 	cPixYCut = cPixY - YminNew
 	cPixZCut = cPixZ - ZminNew
-	
+
 	# update header keywords:
 	header['CRPIX1'] = cPixXCut+1
 	header['CRPIX2'] = cPixYCut+1
 	header['CRPIX3'] = cPixZCut+1
 	
 	# extract the cubelet
-        [ZminNew,ZmaxNew,YminNew,YmaxNew,XminNew,XmaxNew]=map(int,[ZminNew,ZmaxNew,YminNew,YmaxNew,XminNew,XmaxNew])
+	[ZminNew,ZmaxNew,YminNew,YmaxNew,XminNew,XmaxNew]=map(int,[ZminNew,ZmaxNew,YminNew,YmaxNew,XminNew,XmaxNew])
 	subcube = cube[ZminNew:ZmaxNew+1,YminNew:YmaxNew+1,XminNew:XmaxNew+1]
 
 	# update header keywords:
@@ -141,9 +137,9 @@ def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOv
 		sys.stderr.write("ERROR: Output file exists: " + name + ".\n")
 	else:
 		hdulist.writeto(name, output_verify = 'warn', clobber = True)
-	
+
 	hdulist.close()
-	
+
 	# make PV diagram
 	if 'kin_pa' in cathead:
 		pv_sampling=10
@@ -189,7 +185,7 @@ def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOv
 	submask = mask[ZminNew:ZmaxNew+1,YminNew:YmaxNew+1,XminNew:XmaxNew+1].astype('int')
 	submask[submask!=obj[0]] = 0
 	submask[submask==obj[0]] = 1
-	
+
 	# write mask
 	hdu = pyfits.PrimaryHDU(data=submask.astype('int16'),header=header)
 	hdu.header['bunit']='source_ID'
@@ -198,8 +194,8 @@ def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOv
 	hdulist = pyfits.HDUList([hdu])
 	name = outputDir+cubename+'_'+str(int(obj[0]))+'_mask.fits'
 	if compress:
-	  name += '.gz'
-	
+		name += '.gz'
+
 	# Check for overwrite flag:
 	if not flagOverwrite and os.path.exists(name):
 		sys.stderr.write("ERROR: Output file exists: " + name + ".\n")
@@ -209,62 +205,94 @@ def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOv
 	
 
 
-	# moment 0
+	# units of moment images
+	# velocity
 	if 'vopt' in header['ctype3'].lower() or 'vrad' in header['ctype3'].lower() or 'velo' in header['ctype3'].lower() or 'felo' in header['ctype3'].lower():
-		if not 'cunit3' in header: dkms=abs(header['cdelt3'])/1e+3 # assuming m/s
-		elif header['cunit3'].lower()=='m/s': dkms=abs(header['cdelt3'])/1e+3
-		elif header['cunit3'].lower()=='km/s': dkms=abs(header['cdelt3'])
+		if not 'cunit3' in header or header['cunit3'].lower()=='m/s':
+			# converting m/s to km/s
+			dkms=abs(header['cdelt3'])/1e+3
+			scalemom12=1./1e+3
+			bunitExt='.km/s'
+		elif header['cunit3'].lower()=='km/s':
+			dkms=abs(header['cdelt3'])
+			scalemom12=1.
+			bunitExt='.km/s'
+		else:
+			# working with whatever units the cube has
+			dkms=abs(header['cdelt3'])
+			scalemom12=1.
+			bunitExt='.'+header['cunit3']
+	# frequency
 	elif 'freq' in header['ctype3'].lower():
-		if not 'cunit3' in header or header['cunit3'].lower()=='hz': dkms=abs(header['cdelt3'])/1.42040575177e+9*2.99792458e+5 # assuming Hz
-		elif header['cunit3'].lower()=='khz': dkms=abs(header['cdelt3'])/1.42040575177e+6*2.99792458e+5
-		else: dkms=1. # no scaling, avoids crashing
+		if not 'cunit3' in header or header['cunit3'].lower()=='hz':
+			dkms=abs(header['cdelt3'])
+			scalemom12=1.
+			bunitExt='.Hz'
+		elif header['cunit3'].lower()=='khz':
+			# converting kHz to Hz
+			dkms=abs(header['cdelt3'])*1e+3
+			scalemom12=1e+3
+			bunitExt='.Hz'
+		else:
+			# working with whatever units the cube has
+			dkms=abs(header['cdelt3'])
+			scalemom12=1.
+			bunitExt='.'+header['cunit3']
+	# other
+	else:
+		# working with whatever units the cube has
+		dkms=abs(header['cdelt3'])
+		scalemom12=1.
+		if not 'cunit3' in header: bunitExt='.std_unit_'+header['ctype3']
+		else: bunitExt='.'+header['cunit3']
+
+	# make copy of subcube and regrid
 	subcubeCopy = subcube.copy()
 	subcubeCopy[submask==0] = 0
 	if 'cellscal' in header:
 		if header['cellscal'] == '1/F': subcubeCopy = regridMaskedChannels(subcubeCopy,submask,header)
+
+	# moment 0
 	m0=np.nan_to_num(subcubeCopy).sum(axis=0)
 	m0*=dkms
 	hdu = pyfits.PrimaryHDU(data=m0,header=header)
-	hdu.header['bunit']+='.km/s'
+	hdu.header['bunit']+=bunitExt
 	hdu.header['datamin']=np.nanmin(m0)
 	hdu.header['datamax']=np.nanmax(m0)
 	del(hdu.header['crpix3'])
 	del(hdu.header['crval3'])
 	del(hdu.header['cdelt3'])
 	del(hdu.header['ctype3'])
+	if 'cunit3' in hdu.header: del(hdu.header['cunit3'])
 	name = outputDir+cubename+'_'+str(int(obj[0]))+'_mom0.fits'
 	if compress:
-	  name += '.gz'
+		name += '.gz'
 	
 	# Check for overwrite flag:
 	if not flagOverwrite and os.path.exists(name):
 		sys.stderr.write("ERROR: Output file exists: " + name + ".\n")
 	else:
 		hdu.writeto(name,output_verify='warn',clobber=True)
-	
-	
-	# moment 1
-	m1=(np.arange(subcubeCopy.shape[0]).reshape((subcubeCopy.shape[0],1,1))*np.ones(subcubeCopy.shape)-header['crpix3']+1)*header['cdelt3']+header['crval3']
-	if 'vopt' in header['ctype3'].lower() or 'vrad' in header['ctype3'].lower() or 'velo' in header['ctype3'].lower() or 'felo' in header['ctype3'].lower():
-		if not 'cunit3' in header: m1/=1e+3 # assuming m/s
-		elif header['cunit3'].lower()=='km/s': pass
-	elif 'freq' in header['ctype3'].lower():
-		if not 'cunit3' in header or header['cunit3'].lower()=='hz': m1*=2.99792458e+5/1.42040575177e+9 # assuming Hz
-		elif header['cunit3'].lower()=='khz': m1*=2.99792458e+5/1.42040575177e+6
+
+	# prepare mom0 image for mom1 and mom2 calculation
 	m0[m0==0]=np.nan
 	m0/=dkms
+
+	# moment 1
+	m1=((np.arange(subcubeCopy.shape[0]).reshape((subcubeCopy.shape[0],1,1))*np.ones(subcubeCopy.shape)-header['crpix3']+1)*header['cdelt3']+header['crval3'])*scalemom12
 	m1=np.divide(np.array(np.nan_to_num(m1*subcubeCopy).sum(axis=0)),m0)
 	hdu = pyfits.PrimaryHDU(data=m1,header=header)
-	hdu.header['bunit']='km/s'
+	hdu.header['bunit']=bunitExt[1:]
 	hdu.header['datamin']=np.nanmin(m1)
 	hdu.header['datamax']=np.nanmax(m1)
 	del(hdu.header['crpix3'])
 	del(hdu.header['crval3'])
 	del(hdu.header['cdelt3'])
 	del(hdu.header['ctype3'])
+	if 'cunit3' in hdu.header: del(hdu.header['cunit3'])
 	name = outputDir+cubename+'_'+str(int(obj[0]))+'_mom1.fits'
 	if compress:
-	  name += '.gz'
+		name += '.gz'
 	
 	# Check for overwrite flag:
 	if not flagOverwrite and os.path.exists(name):
@@ -274,25 +302,20 @@ def writeSubcube(cube, header, mask, objects, cathead, outroot, compress, flagOv
 	
 	
 	# moment 2
-	m2=(np.arange(subcubeCopy.shape[0]).reshape((subcubeCopy.shape[0],1,1))*np.ones(subcubeCopy.shape)-header['crpix3']+1)*header['cdelt3']+header['crval3']
-	if 'vopt' in header['ctype3'].lower() or 'vrad' in header['ctype3'].lower() or 'velo' in header['ctype3'].lower() or 'felo' in header['ctype3'].lower():
-		if not 'cunit3' in header: m2/=1e+3 # assuming m/s
-		elif header['cunit3'].lower()=='km/s': pass
-	elif 'freq' in header['ctype3'].lower():
-		if not 'cunit3' in header or header['cunit3'].lower()=='hz': m2*=2.99792458e+5/1.42040575177e+9 # assuming Hz
-		elif header['cunit3'].lower()=='khz': m2*=2.99792458e+5/1.42040575177e+6
+	m2=((np.arange(subcubeCopy.shape[0]).reshape((subcubeCopy.shape[0],1,1))*np.ones(subcubeCopy.shape)-header['crpix3']+1)*header['cdelt3']+header['crval3'])*scalemom12
 	m2 = m2**2
 	m2=np.divide(np.array(np.nan_to_num(m2*subcubeCopy).sum(axis=0)),m0)
 	m2-=m1*m1
 	m2=np.sqrt(m2)
 	hdu = pyfits.PrimaryHDU(data=m2,header=header)
-	hdu.header['bunit']='km/s'
-	hdu.header['datamin']=np.nanmin(m1)
-	hdu.header['datamax']=np.nanmax(m1)
+	hdu.header['bunit']=bunitExt[1:]
+	hdu.header['datamin']=np.nanmin(m2)
+	hdu.header['datamax']=np.nanmax(m2)
 	del(hdu.header['crpix3'])
 	del(hdu.header['crval3'])
 	del(hdu.header['cdelt3'])
 	del(hdu.header['ctype3'])
+	if 'cunit3' in hdu.header: del(hdu.header['cunit3'])
 	name = outputDir+cubename+'_'+str(int(obj[0]))+'_mom2.fits'
 	if compress:
 		name += '.gz'