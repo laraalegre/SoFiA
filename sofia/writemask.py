4de63794562099a17d8169f070bba163523c8479
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2017-03-13 05:50:41 +0000

    Cleaned up writemask.py

--- sofia/writemask.py
+++ sofia/writemask.py
@@ -4,57 +4,58 @@ import os
 import sys
 
 def removeOptions(dictionary):
-  modDictionary = dictionary
-  for key in modDictionary['steps']:
-    if modDictionary['steps'][key] == 'False':
-      modDictionary.pop(key.split('do')[1].lower(), None)
-  return modDictionary
+	modDictionary = dictionary
+	for key in modDictionary['steps']:
+		if modDictionary['steps'][key] == 'False':
+			modDictionary.pop(key.split('do')[1].lower(), None)
+	return modDictionary
 
 
-def recursion(dictionary,optionsList,optionsDepth,counter=0):
-  if type(dictionary)==type({}):
-    for k in dictionary:
-      optionsList.append(str(k))
-      optionsDepth.append(counter)
-      recursion(dictionary[k],optionsList,optionsDepth,counter=counter+1)
-  else:
-    optionsList[len(optionsList)-1]+='='+str(dictionary)
-    counter = 0
+def recursion(dictionary, optionsList, optionsDepth, counter=0):
+	if type(dictionary) == type({}):
+		for k in dictionary:
+			optionsList.append(str(k))
+			optionsDepth.append(counter)
+			recursion(dictionary[k], optionsList, optionsDepth, counter=counter+1)
+	else:
+		optionsList[len(optionsList) - 1] += '=' + str(dictionary)
+		counter = 0
 
 
 def writeMask(cube, header, dictionary, filename, compress, flagOverwrite):
-  header.add_history('SOURCE FINDING')
-  optionsList = []
-  optionsDepth = []
-  dictionary = removeOptions(dictionary)
-  recursion(dictionary,optionsList,optionsDepth)
-  headerList = []
-  for i in range(0,len(optionsList)):
-    if len(optionsList[i].split('=')) > 1:
-      tmpString = optionsList[i]
-      depthNumber = optionsDepth[i]
-      j=i-1
-      while depthNumber > 0:
-          if optionsDepth[i]>optionsDepth[j]:
-            tmpString = optionsList[j] + '.' + tmpString
-            depthNumber = optionsDepth[j]
-          j-=1
-      headerList.append(tmpString)
-  for option in headerList:
-    header.add_history(option)
-  if cube.max()<32767: cube=cube.astype('int16')
-  hdu = pyfits.PrimaryHDU(data=cube,header=header)
-  hdu.header['bunit']='source_ID'
-  hdu.header['datamin']=cube.min()
-  hdu.header['datamax']=cube.max()
-  #hdulist = pyfits.HDUList([hdu])
-  #name = os.path.splitext(filename)[0] + '_mask.fits'
-  name = filename
-  if compress:
-    name += '.gz'
-  
-  # Check for overwrite flag:
-  if not flagOverwrite and os.path.exists(name):
-    sys.stderr.write("ERROR: Output file exists: " + name + ".\n")
-  else:
-    hdu.writeto(name,output_verify='warn',clobber=True)
+	header.add_history('SOURCE FINDING')
+	optionsList = []
+	optionsDepth = []
+	dictionary = removeOptions(dictionary)
+	recursion(dictionary,optionsList,optionsDepth)
+	headerList = []
+	for i in range(0, len(optionsList)):
+		if len(optionsList[i].split('=')) > 1:
+			tmpString = optionsList[i]
+			depthNumber = optionsDepth[i]
+			j = i - 1
+			while depthNumber > 0:
+				if optionsDepth[i] > optionsDepth[j]:
+					tmpString = optionsList[j] + '.' + tmpString
+					depthNumber = optionsDepth[j]
+				j -= 1
+			headerList.append(tmpString)
+	for option in headerList:
+		header.add_history(option)
+	if cube.max() < 32767:
+		cube=cube.astype('int16')
+	hdu = pyfits.PrimaryHDU(data=cube, header=header)
+	hdu.header['bunit'] = 'source_ID'
+	hdu.header['datamin'] = cube.min()
+	hdu.header['datamax'] = cube.max()
+	#hdulist = pyfits.HDUList([hdu])
+	#name = os.path.splitext(filename)[0] + '_mask.fits'
+	name = filename
+	if compress:
+		name += '.gz'
+
+	# Check for overwrite flag:
+	if not flagOverwrite and os.path.exists(name):
+		sys.stderr.write("ERROR: Output file exists: " + name + ".\n")
+	else:
+		hdu.writeto(name, output_verify='warn', clobber=True)
