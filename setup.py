1f79cfe8cba30cb6478c50f636d254564083ff61
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2017-11-08 08:36:17 +0000

    Reactivate dependency checks... ...in setup.py and improve the evaluation and reporting of unmet dependencies.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
-from distutils.version import StrictVersion, LooseVersion
+from distutils.version import StrictVersion
 import distutils.command.build
 
 import sys
@@ -15,164 +15,159 @@ from sofia import __version__ as version
 compile_gui = True
 
 # Dependency checking
-dependencies = [['numpy', '1.8'], ['scipy', None]]
+dependencies = [['numpy', '1.8'], ['scipy', '0.7'], ['astropy', '0.2.5']]
+unmetDependencies = False
 
-for (pkg, minversion) in dependencies:
-    try:
-        m = __import__(pkg)
-        if minversion is not None:
-            if StrictVersion(m.__version__) < StrictVersion(minversion):
-                if LooseVersion(m.__version__) < LooseVersion(minversion):
-                    raise ValueError
-                warnings.warn(
-                    'Version', m.__version__,
-                    'of package', pkg,
-                    'might not be sufficient')
-    except ImportError:
-        print 'Package', pkg, 'not present.'
-        sys.exit(1)
-    except ValueError:
-        print 'Package', pkg, 'has version', m.__version__
-        print 'Version', minversion, 'required.'
-        sys.exit(1)
+for (pkg, required_version) in dependencies:
+	try:
+		m = __import__(pkg)
+		available_version = m.__version__
+		if StrictVersion(available_version) < StrictVersion(required_version): raise ValueError
+	except ImportError:
+		print 'ERROR: Package \'' + pkg + '\' not found, but required by SoFiA.'
+		unmetDependencies = True
+	except ValueError:
+		print 'ERROR: Package \'' + pkg + '\' has version number ' + available_version + ","
+		print '       but SoFiA requires version ' + required_version + ' or higher.'
+		unmetDependencies = True
+
+# Exit if unmet dependencies were found
+if unmetDependencies: sys.exit(1)
 
 class build(distutils.command.build.build):
+	user_options = distutils.command.build.build.user_options + [
+		('no-gui=', None, 'Specify if you wish to compile the GUI'),
+		]
 
-    user_options = distutils.command.build.build.user_options + [
-        ('no-gui=', None,
-        'specify if you want the gui to be built'),
-        ]
+	def initialize_options(self, *args, **kwargs):
+		self.no_gui = False
+		distutils.command.build.build.initialize_options(self, *args, **kwargs)
 
-    def initialize_options(self, *args, **kwargs):
-        self.no_gui = False
-        distutils.command.build.build.initialize_options(self, *args, **kwargs)
-
-    def run(self, *args, **kwargs):
-        global nogui
-        nogui = self.no_gui
-        distutils.command.build.build.run(self, *args, **kwargs)
+	def run(self, *args, **kwargs):
+		global nogui
+		nogui = self.no_gui
+		distutils.command.build.build.run(self, *args, **kwargs)
 
 
 
-import numpy
+from numpy import get_include
 
 # Define include directories
 include_dirs = []
 include_dirs.append('src')
 include_dirs.append('src/linker')
-include_dirs.append(numpy.get_include())
+include_dirs.append(get_include())
 
 # Get external include directories from PATH
 ext_include_dirs = []
 ext_library_dirs = []
 try:
-    ext_include_dirs += os.environ['PATH'].split(os.pathsep)
+	ext_include_dirs += os.environ['PATH'].split(os.pathsep)
 except:
-    print 'Could not parse PATH'
-    sys.exit(1)
+	print 'Could not parse PATH'
+	sys.exit(1)
 
 # Get external library paths from LD_LIBRARY_PATH, if it exists
 try:
-    ext_library_dirs += os.environ['LD_LIBRARY_PATH'].split(os.pathsep)
+	ext_library_dirs += os.environ['LD_LIBRARY_PATH'].split(os.pathsep)
 except KeyError:
-    pass
+	pass
 
 # Remove "bin" directories to avoid problems with compiler
-ext_include_dirs = [ext_include_dir for ext_include_dir in ext_include_dirs if not 'bin'
-                        in ext_include_dir]
+ext_include_dirs = [ext_include_dir for ext_include_dir in ext_include_dirs if not 'bin' in ext_include_dir]
 
 # C/C++ source code files
 # Object linking code
 linker_src_base = 'src/linker/'
 linker_src_files = [
-    'linker.cpp',
-    'RJJ_ObjGen_CreateObjs.cpp',
-    'RJJ_ObjGen_DetectDefn.cpp',
-    'RJJ_ObjGen_MemManage.cpp',
-    'RJJ_ObjGen_ThreshObjs.cpp',
-    'RJJ_ObjGen_Dmetric.cpp'
-    ]
+	'linker.cpp',
+	'RJJ_ObjGen_CreateObjs.cpp',
+	'RJJ_ObjGen_DetectDefn.cpp',
+	'RJJ_ObjGen_MemManage.cpp',
+	'RJJ_ObjGen_ThreshObjs.cpp',
+	'RJJ_ObjGen_Dmetric.cpp'
+	]
 linker_src = [linker_src_base + f for f in linker_src_files]
 
 # 2D1D wavelet finder code
 wavelet_src_base = 'src/wavelet/'
 wavelet_src_files = [
-    'wavelet.c'
-    ]
+	'wavelet.c'
+	]
 wavelet_src = [wavelet_src_base + f for f in wavelet_src_files]
 
 # CNHI finder code
 CNHI_src_base = 'src/CNHI/'
 CNHI_src_files = [
-    'CNHI.cpp'
-    ]
+	'CNHI.cpp'
+	]
 CNHI_src = [CNHI_src_base + f for f in CNHI_src_files]
 
 # moment output module
 writemoment2_src_base = 'src/writemoment2/'
 writemoment2_src_files = [
-    'writemoment2.c'
-    ]
+	'writemoment2.c'
+	]
 writemoment2_src = [writemoment2_src_base + f for f in writemoment2_src_files]
 
 # Interface to the parametrization code
 parametrizer_src_base = 'src/parametrizer/'
 parametrizer_src_files = [
-    'BFfit.cpp',
-    'cparametrizer.cpp',
-    'DataCube.cpp',
-    'helperFunctions.cpp',
-    'MaskOptimization.cpp',
-    'Measurement.cpp',
-    'MetaData.cpp',
-    'ModuleParametrisation.cpp',
-    'Parametrization.cpp',
-    'Source.cpp',
-    'SourceCatalog.cpp',
-    'Unit.cpp',
-    ]
+	'BFfit.cpp',
+	'cparametrizer.cpp',
+	'DataCube.cpp',
+	'helperFunctions.cpp',
+	'MaskOptimization.cpp',
+	'Measurement.cpp',
+	'MetaData.cpp',
+	'ModuleParametrisation.cpp',
+	'Parametrization.cpp',
+	'Source.cpp',
+	'SourceCatalog.cpp',
+	'Unit.cpp',
+	]
 parametrizer_src = [parametrizer_src_base + f for f in parametrizer_src_files]
 
 setup(
-    name='sofia',
-    version=version,
-    ext_package='sofia',
-    ext_modules=[
-        Extension(
-            'linker',
-            linker_src,
-            extra_compile_args=['-O3'],
-            include_dirs=include_dirs),
-        Extension(
-            'wavelet',
-            wavelet_src,
-            extra_compile_args=['-O3'],
-            include_dirs=include_dirs),
-        Extension(
-            'CNHI',
-            CNHI_src,
-            extra_compile_args=['-I. -O3'],
-            include_dirs=include_dirs),
-        Extension(
-            'writemoment2',
-            writemoment2_src,
-            extra_compile_args=['-O3'],
-            include_dirs=include_dirs),
-        Extension(
-            'cparametrizer',
-            parametrizer_src,
-            extra_compile_args=['-O3'],
-            include_dirs=include_dirs + ext_include_dirs,
-            library_dirs=ext_library_dirs + ext_include_dirs#,
-            #libraries=['gsl', 'gslcblas']
-            )
-        ],
-    package_dir={'sofia': 'sofia'},
-    packages=['sofia'],  #,"cfitsio", "wcs"
-    cmdclass={
-        'build': build,
-    },
-    )
+	name='sofia',
+	version=version,
+	ext_package='sofia',
+	ext_modules=[
+		Extension(
+			'linker',
+			linker_src,
+			extra_compile_args=['-O3'],
+			include_dirs=include_dirs),
+		Extension(
+			'wavelet',
+			wavelet_src,
+			extra_compile_args=['-O3'],
+			include_dirs=include_dirs),
+		Extension(
+			'CNHI',
+			CNHI_src,
+			extra_compile_args=['-I. -O3'],
+			include_dirs=include_dirs),
+		Extension(
+			'writemoment2',
+			writemoment2_src,
+			extra_compile_args=['-O3'],
+			include_dirs=include_dirs),
+		Extension(
+			'cparametrizer',
+			parametrizer_src,
+			extra_compile_args=['-O3'],
+			include_dirs=include_dirs + ext_include_dirs,
+			library_dirs=ext_library_dirs + ext_include_dirs#,
+			#libraries=['gsl', 'gslcblas']
+			)
+		],
+	package_dir={'sofia': 'sofia'},
+	packages=['sofia'],  #,"cfitsio", "wcs"
+	cmdclass={
+		'build': build,
+	},
+	)
 
 # make sofia_pipeline.py executable
 os.chmod('sofia_pipeline.py', os.stat('sofia_pipeline.py').st_mode | stat.S_IXUSR)
@@ -180,8 +175,8 @@ os.chmod('sofia_pipeline.py', os.stat('sofia_pipeline.py').st_mode | stat.S_IXUS
 # path to the sofia modules
 cwd = os.getcwd()
 for ll in os.listdir(cwd + '/build'):
-    if 'lib.' in ll:
-        sofiaModulesPath = cwd + '/build/' + ll
+	if 'lib.' in ll:
+		sofiaModulesPath = cwd + '/build/' + ll
 
 # set system variable
 os.environ['SOFIA_PIPELINE_PATH'] = cwd + '/sofia_pipeline.py'
@@ -193,21 +188,21 @@ if nogui=='True': compile_gui = False
 else: compile_gui = True
 
 if compile_gui:
-    os.chdir('gui')
-    os.system('qmake; make')
-    if sys.platform == 'darwin':
-        os.chmod(
-            'SoFiA.app/Contents/MacOS/SoFiA',
-            os.stat('SoFiA.app/Contents/MacOS/SoFiA').st_mode | stat.S_IXUSR
-            )
-        sofiaApplicationPath = cwd + '/gui/SoFiA.app/Contents/MacOS'
-    else:
-        os.chmod(
-            'SoFiA',
-            os.stat('SoFiA').st_mode | stat.S_IXUSR
-            )
-        sofiaApplicationPath = cwd + '/gui'
-    os.chdir('../')
+	os.chdir('gui')
+	os.system('qmake; make')
+	if sys.platform == 'darwin':
+		os.chmod(
+			'SoFiA.app/Contents/MacOS/SoFiA',
+			os.stat('SoFiA.app/Contents/MacOS/SoFiA').st_mode | stat.S_IXUSR
+			)
+		sofiaApplicationPath = cwd + '/gui/SoFiA.app/Contents/MacOS'
+	else:
+		os.chmod(
+			'SoFiA',
+			os.stat('SoFiA').st_mode | stat.S_IXUSR
+			)
+		sofiaApplicationPath = cwd + '/gui'
+	os.chdir('../')
 
 print '\n-------------------------------------------------------------------------\n'
 print '\033[1;32mInstallation complete.\033[0m\n'
