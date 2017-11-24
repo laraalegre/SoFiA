188e2f02c4e1c424f370dd1abdbbbd64874e13fe
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2017-11-24 02:48:33 +0000

    Cleaned up sigma_cube.py Including: removal of unused variables; removal of comparisons with literals True and False; replacement of print() with sys.stdout.write(); addition of warning messages; general formatting of code.

--- sofia/sigma_cube.py
+++ sofia/sigma_cube.py
@@ -1,84 +1,76 @@
 #! /usr/bin/env python
 
-# import default python libraries
 import numpy as np
 import scipy as sp
 from .functions import *
 import sys
 
-# function to read in a cube and generate a cube with sigma levels 
-
-# this script is useful to correct for variation in noise as function of frequency, noisy edges of cubes and channels with strong RFI
+# Function to read in a cube and scale it by the RMS.
+# This script is useful to correct for variation in noise as function of frequency, noisy edges of cubes and channels with strong RFI.
 
 def sigma_scale(cube, scaleX=False, scaleY=False, scaleZ=True, edgeX=0, edgeY=0, edgeZ=0, statistic="mad"):
 	verbose = 0
 	
-	# sigma scaling only works for 3D cubes, as it is mainly designed to correct for differences in frequency
+	# Sigma scaling only works for 3D cubes, as it is mainly designed to correct for differences in frequency.
 	
-	# cube: the input cube
-	# edgeX,Y,Z: the edges of the cube that can are excluded for the noise calculation (default 0,0,0)
-	# statistic: mad or std (default mad)
+	# cube:       the input cube
+	# edgeX,Y,Z:  the edges of the cube that are excluded from the noise calculation (default: 0,0,0)
+	# statistic:  "mad" or "std" (default: "mad")
 	
-	print ('Start generating Sigma-value cube')
+	sys.stdout.write('Generating noise-scaled data cube:\n')
 	
-	if statistic == 'mad': print ('Apply Median Absolute Deviation (MAD) statistic')
-	if statistic == 'std': print ('Apply Standard Deviation (STD) statistic')
-	if statistic == 'negative': print ('Apply Negatice statistic')
+	if statistic == 'mad': sys.stdout.write('Applying Median Absolute Deviation (MAD) statistic.\n')
+	if statistic == 'std': sys.stdout.write('Applying Standard Deviation (STD) statistic.\n')
+	if statistic == 'negative': sys.stdout.write('Applying Negative statistic.\n')
 	sys.stdout.flush()
 	
-	edge_x1 = edgeX # the edge of the cube which is trimmed off when calculating the statistics
-	edge_x2 = edgeX
-	edge_y1 = edgeY
-	edge_y2 = edgeY
-	edge_z1 = edgeZ
-	edge_z2 = edgeZ
-	
-	# check the dimensions of the cube (could be obtained from header information)
+	# Check the dimensions of the cube (could be obtained from header information)
 	dimensions = np.shape(cube)
 	
-	# define the range over which statistics are calculated
-	z1 = edge_z1
-	z2 = dimensions[0] - edge_z2
-	y1 = edge_y1
-	y2 = dimensions[1] - edge_y2
-	x1 = edge_x1
-	x2 = dimensions[2] - edge_x2
+	# Define the range over which statistics are calculated
+	z1 = edgeZ
+	z2 = dimensions[0] - edgeZ
+	y1 = edgeY
+	y2 = dimensions[1] - edgeY
+	x1 = edgeX
+	x2 = dimensions[2] - edgeX
 	
-	if scaleZ == True:
+	if scaleZ:
 		z_rms = np.zeros(dimensions[0])
 		for i in range(len(z_rms)):
-			if (not np.isnan(cube[i, y1:y2, x1:x2])).sum():
+			if np.all(np.isnan(cube[i,y1:y2,x1:x2])):
+				z_rms[i] = 0
+				sys.stderr.write("WARNING: No valid data found in plane at z = " + str(i + 1) + "\n")
+			else:
 				z_rms[i] = GetRMS(cube[i, y1:y2, x1:x2], rmsMode=statistic, zoomx=1, zoomy=1, zoomz=1, verbose=verbose)
-			else: z_rms[i] = 0
-		# scale the cube by the rms
+		# Scale the cube by the rms
 		for i in range(len(z_rms)):
-			if z_rms[i] > 0:
-				cube[i,:,:] = cube[i,:,:] / z_rms[i]
+			if z_rms[i] > 0: cube[i,:,:] = cube[i,:,:] / z_rms[i]
 	
-	if scaleY == True:
+	if scaleY:
 		y_rms = np.zeros(dimensions[1])
 		for i in range(len(y_rms)):
-			if (not np.isnan(cube[z1:z2, i, x1:x2])).sum():
+			if np.all(np.isnan(cube[z1:z2,i,x1:x2])):
+				y_rms[i] = 0
+				sys.stderr.write("WARNING: No valid data found in plane at y = " + str(i + 1) + "\n")
+			else:
 				y_rms[i] = GetRMS(cube[z1:z2, i, x1:x2], rmsMode=statistic, zoomx=1, zoomy=1, zoomz=1, verbose=verbose)
-			else: y_rms[i] = 0
-	
-		# scale the cube by the rms
+		# Scale the cube by the rms
 		for i in range(len(y_rms)):
-			if y_rms[i] > 0:
-				cube[:,i,:] = cube[:,i,:] / y_rms[i]
+			if y_rms[i] > 0: cube[:,i,:] = cube[:,i,:] / y_rms[i]
 	
-	if scaleX == True:
+	if scaleX:
 		x_rms = np.zeros(dimensions[2])
 		for i in range(len(x_rms)):
-			if (not np.isnan(cube[z1:z2, y1:y2, i])).sum():
+			if np.all(np.isnan(cube[z1:z2,y1:y2,i])):
+				x_rms[i] = 0
+				sys.stderr.write("WARNING: No valid data found in plane at x = " + str(i + 1) + "\n")
+			else:
 				x_rms[i] = GetRMS(cube[z1:z2, y1:y2, i], rmsMode=statistic, zoomx=1, zoomy=1, zoomz=1, verbose=verbose)
-			else: x_rms[i] = 0
-		# scale the cube by the rms
+		# Scale the cube by the rms
 		for i in range(len(x_rms)):
-			if x_rms[i] > 0:
-				cube[:,:,i] = cube[:,:,i] / x_rms[i]
+			if x_rms[i] > 0: cube[:,:,i] = cube[:,:,i] / x_rms[i]
 	
-	print ('Sigma cube is created')
-	print
+	sys.stdout.write('Noise-scaled data cube generated.\n\n')
 	sys.stdout.flush()
 	return cube
