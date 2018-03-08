Fixed bug in GetRMS()... ...to check if negative or positive values are actually present for RMS calculation and exit gracefully if not.

sofia/functions.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import scipy as sp
 from distutils.version import StrictVersion, LooseVersion
-import error
+from sofia import error as err
 
 # Check numpy and scipy version numbers for the nanmedian function import
 if LooseVersion(np.__version__) >= LooseVersion("1.9.0"):
@@ -55,17 +55,21 @@ def GetRMS(cube, rmsMode="negative", fluxRange="all", zoomx=1, zoomy=1, zoomz=1,
 	# Check if only negative or positive pixels are to be used:
 	if fluxRange == "negative":
 		subCube = cube[cube[z0:z1:sample, y0:y1:sample, x0:x1:sample] < 0]
+		err.ensure(subCube.size, "Cannot measure noise from negative flux values.\nNo negative fluxes found in data cube.")
 		subCube[::2] *= -1   # Flip the sign of every other element
 	elif fluxRange == "positive":
 		subCube = cube[cube[z0:z1:sample, y0:y1:sample, x0:x1:sample] > 0]
+		err.ensure(subCube.size, "Cannot measure noise from positive flux values.\nNo positive fluxes found in data cube.")
 		subCube[::2] *= -1   # Flip the sign of every other element
 	
 	
 	# GAUSSIAN FIT TO NEGATIVE FLUXES
 	if rmsMode == "negative":
 		nrbins = max(100, int(math.ceil(float(cube.size) / 1e+5)))
+		
 		cubemin = np.nanmin(cube)
-		ensure(cubemin < 0, "Cannot estimate noise from Gaussian fit to negative flux\nhistogram; no negative fluxes found in data cube.")
+		err.ensure(cubemin < 0, "Cannot estimate noise from Gaussian fit to negative flux\nhistogram; no negative fluxes found in data cube.")
+		
 		bins = np.arange(cubemin, abs(cubemin) / nrbins - 1e-12, abs(cubemin) / nrbins)
 		fluxval = (bins[:-1] + bins[1:]) / 2
 		rmshisto = np.histogram(cube[z0:z1:sample, y0:y1:sample, x0:x1:sample][~np.isnan(cube[z0:z1:sample, y0:y1:sample, x0:x1:sample])], bins=bins)[0]