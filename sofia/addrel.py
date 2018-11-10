2ff914c05445afa297446466ff0648be41683c5d
paoloserra <paolo.serra@inaf.it>
2018-11-10 18:37:57 +0200

    check that negPerBin is >= 1

--- sofia/addrel.py
+++ sofia/addrel.py
@@ -23,6 +23,9 @@ class gaussian_kde_set_covariance(stats.gaussian_kde):
 # ================================================
 
 def EstimateRel(data, pdfoutname, parNames, parSpace=["snr_sum", "snr_max", "n_pix"], logPars=[1, 1, 1], autoKernel=True, scaleKernel=1, negPerBin=1, skellamTol=-0.5, kernel=[0.15, 0.05, 0.1], usecov=False, doscatter=1, docontour=1, doskellam=1, dostats=0, saverel=1, threshold=0.99, fMin=0, verb=0, makePlot=False):
+	# Set negPerBin to be >=1
+	negPerBin=max(1.,negPerBin)
+	
 	# Import Matplotlib if diagnostic plots requested
 	if makePlot:
 		import matplotlib
